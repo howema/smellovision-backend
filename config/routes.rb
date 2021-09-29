4c245b458522d98b16e05c41d7f2edcf82dd5fe0
MEG HOWE <howem9@gmail.com>
2021-09-29 13:43:28 -0400

    adds controller actions for join tables

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,16 @@ Rails.application.routes.draw do
   get "/genres" => "genres#index"
 
   post "/sessions" => "sessions#create"
+
+  get "/actorshows" => "actor_shows#index"
+  post "/actorshows" => "actor_shows#create"
+  get "/actorshows/:id" => "actor_shows#show"
+  patch "/actorshows/:id" => "actor_shows#update"
+  delete "/actorshows/:id" => "actor_shows#destroy"
+
+  get "/showgenres" => "show_genres#index"
+  post "/showgenres" => "show_genres#create"
+  get "/showgenres/:id" => "show_genres#show"
+  patch "/showgenres/:id" => "show_genres#update"
+  delete "/showgenres/:id" => "show_genres#destroy"
 end
